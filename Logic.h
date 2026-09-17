7d385f2fffc2a040f9f8ecb30de21d645a3f6e2e
JoseIgnacio12 <j.murillo.12@estudiantec.cr>
2026-09-17 10:30:29 -0600

    En Horarios_choque se crea la funcion de detectar_choque_catalogo,evaluar_elegibildad_curs. Y también se crea Logic.H donde se guardan las funciones de Horarios_Choques

---
+++ Logic.h
@@ -0,0 +1,11 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+#include "models.h"
+
+int horas_minutos(int horas, int minutos);
+int horarios_choque(const Horario *h1, const Horario *h2);
+int grupo_choque(const Grupo *Gr1, const Grupo *Gr2);
+void detectar_choques_catalogo(Curso catalogo[], int total_cursos);
+void evaluar_elegibilidad_curso(Curso *curso, char historial[][MAX_CODIGO], int total_aprobados);
+
+#endif
