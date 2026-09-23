d5b2e332954d18cf668a5394925411e30ee1fc9a
Sebastián <playsebastiangm09@gmail.com>
2026-09-22 18:09:10 -0600

    Agregación al struct Curso el char razon_no_matriculable

--- constants.h
+++ constants.h
@@ -7,7 +7,7 @@
 #define MAX_PROFESOR 60        // ej. "Schmidt Peralta Jeff"
 
 // --- Límites por curso/grupo (arreglos fijos: un curso no tiene decenas de grupos) ---
-#define MAX_GRUPOS_POR_CURSO 6
+#define MAX_GRUPOS_POR_CURSO 50
 #define MAX_BLOQUES_POR_GRUPO 4     // bloques de horario por grupo (ej. lun+mie+vie = 3)
 #define MAX_REQUISITOS 5
 #define MAX_CORREQUISITOS 5
